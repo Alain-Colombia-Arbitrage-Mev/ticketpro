3a69dfc109b02fe29637c353cfd78cc4862c4ccd
Alain Colombia <alain@colombia.mev>
2025-11-14 17:38:25 -0500

    Fix pantalla en negro: remover RouterProvider inexistente y renderizar App directamente

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,14 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
 import App from "./App.tsx";
-import { RouterProvider } from "./components/common/Router";
+
 import "./index.css";
 import "./styles/globals.css";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <HelmetProvider>
-      <RouterProvider>
-    <App />
-      </RouterProvider>
+      <App />
     </HelmetProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
